ccf33f42844dc419737cba2e6214f5f88f4d804d
Jiko <youstinahabib422@gmail.com>
2026-04-21 17:09:40 +0200

    on generate routes

---
+++ lib/core/helper_functions/ongenerate_routes.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+import 'package:grocify/features/splash/splash_view.dart';
+
+Route<dynamic>? onGenerateRoutes(RouteSettings settings) {
+  switch (settings.name) {
+
+    case SplashView.routeName:
+      return MaterialPageRoute(
+        builder: (_) => const SplashView(),
+        settings: settings,
+      );
+
+    // case OnboardingView.routeName:
+    //   return MaterialPageRoute(
+    //     builder: (_) => const OnboardingView(),
+    //     settings: settings,
+    //   );
+
+    // case LoginView.routeName:
+    //   return MaterialPageRoute(
+    //     builder: (_) => const LoginView(),
+    //     settings: settings,
+    //   );
+
+    // case MainLayout.routeName:
+    //   return MaterialPageRoute(
+    //     builder: (_) => const MainLayout(),
+    //     settings: settings,
+    //   );
+
+    // case HomeView.routeName:
+    //   return MaterialPageRoute(
+    //     builder: (_) => const HomeView(),
+    //     settings: settings,
+    //   );
+
+    // case ErasView.routeName:
+    //   return MaterialPageRoute(
+    //     builder: (_) => const ErasView(),
+    //     settings: settings,
+    //   );
+
+    // case FigureView.routeName:
+    //   final String eraName = settings.arguments as String;
+    //   return MaterialPageRoute(
+    //     builder: (_) => FigureView(
+    //       eraName: eraName,
+    //     ),
+    //     settings: settings,
+    //   );
+
+    // case DisplayDetailView.routeName:
+    //   final FigureEntity figure = settings.arguments as FigureEntity;
+    //   return PageRouteBuilder(
+    //     settings: settings,
+    //     transitionDuration: const Duration(milliseconds: 900),
+    //     reverseTransitionDuration: const Duration(milliseconds: 900),
+    //     pageBuilder: (_, animation, __) => DisplayDetailView( figure: figure),
+    //     transitionsBuilder: (_, animation, __, child) {
+    //       return FadeTransition(
+    //         opacity: Tween<double>(
+    //           begin: 0.0,
+    //           end: 1.0,
+    //         ).animate(
+    //           CurvedAnimation(
+    //             parent: animation,
+    //             curve: const Interval(0.2, 1.0),
+    //           ),
+    //         ),
+    //         child: child,
+    //       );
+    //     },
+    //   );
+
+    // case WeatherView.routeName:
+    //   return MaterialPageRoute(
+    //     builder: (_) => const WeatherView(),
+    //     settings: settings,
+    //   );
+
+    // case ChatbotView.routeName:
+    //   return MaterialPageRoute(
+    //     builder: (_) => const ChatbotView(),
+    //     settings: settings,
+    //   );
+
+    // case VrInstructionsView.routeName:
+    //   return MaterialPageRoute(
+    //     builder: (_) => const VrInstructionsView(),
+    //     settings: settings,
+    //   );
+
+    // case ProfileView.routeName:
+    //   return MaterialPageRoute(
+    //     builder: (_) => const ProfileView(),
+    //     settings: settings,
+    //   );
+
+    default:
+      return null;
+  }
+}
