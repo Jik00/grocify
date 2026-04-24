e04ebc14a8ab9be59a0d6219af8aa12da46440b4
Jiko <youstinahabib422@gmail.com>
2026-04-25 00:08:51 +0300

    after auth -2- - adjust nav through: - splash nav manger to remove logic from ui, it get auth status from repo + kIsOnboardingSeen - don't from your basic normal nav in the 1st time, the sign in/up consumer navigate to home if success state

--- lib/features/auth/presentation/views/widgets/signup_view_body_bloc_consumer.dart
+++ lib/features/auth/presentation/views/widgets/signup_view_body_bloc_consumer.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:grocify/core/services/navigation_service.dart';
 import 'package:grocify/core/widgets/custom_loading_bar.dart';
-import 'package:grocify/features/auth/presentation/cubits/signup_cubit/signup_cubit.dart';
+import 'package:grocify/features/auth/presentation/manger/signup_cubit/signup_cubit.dart';
 import 'package:grocify/features/auth/presentation/views/widgets/customized_ui.dart';
 import 'package:grocify/features/auth/presentation/views/widgets/signup_view_body.dart';
+import 'package:grocify/features/home/presentation/views/main_layout.dart';
 import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 import '../../../../../core/widgets/build_error_snack_bar.dart';
@@ -17,6 +19,9 @@ class SignUpViewBodyBlocConsumer extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocConsumer<SignupCubit, SignupState>(
       listener: (context, state) {
+        if (state is SignupSuccess) {
+          NavigationService.navigateToGlobal( MainLayout.routeName);
+        }
         if (state is SignupFailure) {
           buildErrorSnackBar(context, state.message);
         }
---
+++ lib/features/splash/presentation/manager/splash_nav_manager.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:grocify/core/services/shared_preferences_singleton.dart';
+import 'package:grocify/core/utils/constants.dart';
+import 'package:grocify/features/auth/domain/repos/auth_repo.dart';
+import 'package:grocify/features/auth/presentation/views/signin_view.dart';
+import 'package:grocify/features/home/presentation/views/main_layout.dart';
+import 'package:grocify/features/onboarding/presentation/views/onboarding_view.dart';
+
+class SplashNavManager {
+  final AuthRepo _authRepo;
+
+
+  static const minDisplayDuration = Duration(seconds: 4);
+
+  SplashNavManager(this._authRepo);
+
+
+  Future<String> determineNextRoute() async {
+    // Ensure minimum splash duration
+    await Future.delayed(minDisplayDuration);
+
+    final isOnboardingSeen = Prefs.getBool(kIsOnboardingSeen);
+
+    if (!isOnboardingSeen) {
+      return OnboardingView.routeName;
+    }
+
+    // Check current auth state from repo
+    final currentUser = await _authRepo.getCurrentUser();
+    final isAuthenticated = currentUser != null;
+
+    if (isAuthenticated) {
+      return MainLayout.routeName;
+    }
+
+    return SignInView.routeName;
+  }
+
+}
+
+extension SplashNavigationManagerExtension on BuildContext {
+  SplashNavManager get splashNavigationManager =>
+      SplashNavManager(read<AuthRepo>());
+}
