e91178f037df02f03ad38a36920cb91b204d5441
Jiko <youstinahabib422@gmail.com>
2026-05-09 18:51:28 +0300

    cart -5- - load cart fun - map products with ids to hydrate data - iterate by row & assign the type entity

--- lib/features/cart/data/cart_repo_impl.dart
+++ lib/features/cart/data/cart_repo_impl.dart
@@ -2,8 +2,11 @@ import 'dart:developer';
 
 import 'package:dartz/dartz.dart';
 import 'package:grocify/core/data_sources/supa_data_source.dart';
+import 'package:grocify/core/entities/product_entity.dart';
 import 'package:grocify/core/errors/failures.dart';
 import 'package:grocify/core/utils/constants.dart';
+import 'package:grocify/features/cart/domain/entities/all_cart_entity.dart';
+import 'package:grocify/features/cart/domain/entities/cart_item_entity.dart';
 import 'package:grocify/features/cart/domain/entities/repos/cart_repo.dart';
 
 class CartRepoImpl extends CartRepo {
@@ -12,7 +15,8 @@ class CartRepoImpl extends CartRepo {
   CartRepoImpl({required this.supabaseDataSource});
 
   @override
-  Future<Either<Failure, void>> addToCart(String productId, String userId)  async{
+  Future<Either<Failure, void>> addToCart(
+      String productId, String userId) async {
     try {
       log("adding to cart");
       await supabaseDataSource.addData(
@@ -39,16 +43,33 @@ class CartRepoImpl extends CartRepo {
   }
 
   @override
-  Future<Either<Failure, Set<String>>> loadCart(String userId) async {
+  Future<Either<Failure, AllCartEntity>> loadCart(String userId) async {
     try {
       log("loading cart");
       final response = await supabaseDataSource.fetchDataBy(
           tableName: kSupaCartTable, query: kUserIdQuery, value: userId);
 
-      final cart = response.map((e) => e[kProductIdQuery] as String).toSet();
+      final Map<String, ProductEntity> productsMap = {
+        for (final p in productsEntities) p.id: p,
+      };
 
-      log("fetched ${cart.length} cart items");
-      return Right(cart);
+      final cartItems = response.map((row) {
+        final productId = row[kProductIdQuery] as String;
+        final product = productsMap[productId];
+        final quantity = row[kQuantityQuery] as int;
+
+        if (product == null) return null;
+
+        return CartItemEntity(
+          product: product,
+          count: quantity,
+        );
+      }).whereType<CartItemEntity>().toList();
+
+      log("fetched ${cartItems.length} cartItems cart items");
+
+
+      return Right(AllCartEntity(cartItems: cartItems));
     } catch (e) {
       log("error loading cart $e");
       return Left(ServerFailure('Failed to load cart: ${e.toString()}'));
@@ -56,7 +77,8 @@ class CartRepoImpl extends CartRepo {
   }
 
   @override
-  Future<Either<Failure, void>> removeFromCart(String productId, String userId) async {
+  Future<Either<Failure, void>> removeFromCart(
+      String productId, String userId) async {
     try {
       log("removing from cart");
       await supabaseDataSource.deleteData(
@@ -71,9 +93,10 @@ class CartRepoImpl extends CartRepo {
       return Left(ServerFailure('Failed to remove from cart: ${e.toString()}'));
     }
   }
-  
+
   @override
-  Future<Either<Failure, void>> updateCart(String productId, String userId, String quantity) async {
+  Future<Either<Failure, void>> updateCart(
+      String productId, String userId, String quantity) async {
     try {
       log("updating cart");
       await supabaseDataSource.updataData(
@@ -82,16 +105,11 @@ class CartRepoImpl extends CartRepo {
           value: userId,
           query2: kProductIdQuery,
           value2: productId,
-
           newData: {kQuantityQuery: quantity});
       return const Right(null);
     } catch (e) {
       log("error updating cart $e");
       return Left(ServerFailure('Failed to update cart: ${e.toString()}'));
     }
-    
   }
-
-  
-  
 }
--- lib/features/cart/domain/entities/repos/cart_repo.dart
+++ lib/features/cart/domain/entities/repos/cart_repo.dart
@@ -1,8 +1,9 @@
 import 'package:dartz/dartz.dart';
 import 'package:grocify/core/errors/failures.dart';
+import 'package:grocify/features/cart/domain/entities/all_cart_entity.dart';
 
 abstract class CartRepo {
-  Future<Either<Failure, Set<String>>> loadCart(String userId);
+  Future<Either<Failure, AllCartEntity>> loadCart(String userId);
   Future<Either<Failure, void>> addToCart(String productId, String userId);
   Future<Either<Failure, void>> updateCart(String productId, String userId, String quantity);
   Future<Either<Failure, void>> removeFromCart(String productId, String userId);
